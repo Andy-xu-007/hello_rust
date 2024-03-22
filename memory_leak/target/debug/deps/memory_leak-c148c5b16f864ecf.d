54903357317f9748f9a75f9fab6a037751f42c7c
andy <changan.xu@tendcloud.com>
2024-03-22 15:56:05 +0800

    smart_pointer

---
+++ memory_leak/target/debug/deps/memory_leak-c148c5b16f864ecf.d
@@ -0,0 +1,5 @@
+/Users/xilin/andy/github/hello_rust/memory_leak/target/debug/deps/libmemory_leak-c148c5b16f864ecf.rmeta: src/main.rs
+
+/Users/xilin/andy/github/hello_rust/memory_leak/target/debug/deps/memory_leak-c148c5b16f864ecf.d: src/main.rs
+
+src/main.rs:
